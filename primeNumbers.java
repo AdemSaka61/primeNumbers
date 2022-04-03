6bac6e44907e127fecfa7d1be48dc78d709a3bed
Edimain <101714041+Edimain@users.noreply.github.com>
2022-04-03 14:45:13 +0300

    Add files via upload

---
+++ primeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class primeNumbers {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int sayac = 0;
+
+        for (int i = 2; i <= 100; i++) {
+            int kontrol = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    kontrol = 1;
+                    break;
+                }
+
+            }
+            if (kontrol == 0) {
+                System.out.println(i + "\n");
+                sayac++;
+
+            }
+        }
+    }
+}
